fe275ab9c5371496c59e3af70f68ee2eb1f93245
‘KeranLi’ <keranli98@outlook.com>
2023-10-23 16:52:13 +0800

    Modified the Main.py and run it rely on argparse

--- Main.py
+++ Main.py
@@ -2,15 +2,35 @@
 # ------------------------------------------------------------------------------------ #
 # Author: Keran Li, Nanjing University, keranli98@outlook.comy
 # This module is mainly designed to input must information
+# Use add parse to run code on the terminal
 # ------------------------------------------------------------------------------------ #
 
+import argparse
 from TableProcess import import_merge_data, export_combined_data
 
-folder_path = 'unprocessed_datasets/'  # Input files' direction. Here, we just need to input the fold name because all files in the fold will be input
-output_path = 'combined_data.xlsx'  # Output file's direction. Here, the default direction is same with the "Main.py"
+def main():
 
-# Import and merge data file
-combined_data = import_merge_data(folder_path)
+    # Set up the argparse interprets
+    parser = argparse.ArgumentParser(description="Combine and export data from multiple Excel files")
 
-# Export data file
-export_combined_data(combined_data, output_path)
+    # Add parameters
+    """
+    --input_folder, the folder that you want to merge
+    --output_file, the folder that you want to store results
+    """
+    parser.add_argument('--input_folder', '-i', type=str, default='./experimental_datasets/',
+                        help="Input files' directory")
+    parser.add_argument('--output_file', '-o', type=str, default='./results/experimental_combined_data.xlsx',
+                        help="Output file's name")
+    
+    # Parameters backles up
+    args = parser.parse_args()
+
+    # Import some pre-defined functions
+    combined_data = import_merge_data(args.input_folder)
+    export_combined_data(combined_data, args.output_file)
+
+    print(f"Exported data to: {args.output_file}")
+
+if __name__ == '__main__':
+    main()
